7a6ff25b2b4002f2f67572db97b50d9f5838f3b0
ma9o <magogagiovanni@gmail.com>
2023-09-06 07:04:29 -0400

    Add chrome flags

--- apps/vnc-input-bridge/src/startSession.ts
+++ apps/vnc-input-bridge/src/startSession.ts
@@ -1,9 +1,15 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import PrefsPlugin from 'puppeteer-extra-plugin-user-preferences';
 import { WebSocket } from 'ws';
+
+puppeteer.use(StealthPlugin());
+puppeteer.use(PrefsPlugin());
 
 export async function startSession(ws: WebSocket) {
   // Launch the browser
   const browser = await puppeteer.launch({
+    executablePath: '/usr/bin/chromium-browser',
     headless: false,
     args: [
       '--start-fullscreen',
@@ -12,6 +18,7 @@ export async function startSession(ws: WebSocket) {
       '--disable-session-crashed-bubble',
       '--noerrdialogs',
     ],
+    ignoreDefaultArgs: ['--enable-automation'],
   });
 
   // Create a page
