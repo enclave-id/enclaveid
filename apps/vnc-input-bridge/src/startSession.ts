3be39edaa61d94c45e684ed50fbd518c69f6c719
ma9o <magogagiovanni@gmail.com>
2023-09-08 10:54:22 -0400

    Update bouding boxes

--- apps/vnc-input-bridge/src/startSession.ts
+++ apps/vnc-input-bridge/src/startSession.ts
@@ -1,4 +1,4 @@
-import { KnownDevices } from 'puppeteer';
+import { Page } from 'puppeteer';
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import PrefsPlugin from 'puppeteer-extra-plugin-user-preferences';
@@ -7,8 +7,32 @@ import { WebSocket } from 'ws';
 puppeteer.use(StealthPlugin());
 puppeteer.use(PrefsPlugin());
 
+async function updateBoundingBoxes(page: Page, ws: WebSocket) {
+  await page.waitForSelector('input[type="email"]');
+  await page.waitForSelector('input[type="password"]');
+
+  const elements = await page.$$('input[type="email"], input[type="password"]');
+
+  const boundingBoxes = await Promise.all(
+    elements.map(async (element) => {
+      return element.boundingBox();
+    })
+  );
+
+  boundingBoxes
+    .filter((el) => el)
+    .forEach((bb) => {
+      const bufferLike = Buffer.from(JSON.stringify(bb));
+      ws?.send(bufferLike);
+    });
+
+  page.on('framenavigated', async (frame) => {
+    const newPage = frame.page();
+    await updateBoundingBoxes(newPage, ws);
+  });
+}
+
 export async function startSession(ws: WebSocket, vh: number, vw: number) {
-  // Launch the browser
   const browser = await puppeteer.launch({
     executablePath: '/usr/bin/chromium-browser',
     headless: false,
@@ -27,30 +51,11 @@ export async function startSession(ws: WebSocket, vh: number, vw: number) {
     },
   });
 
-  // Create a page
   const page = await browser.newPage();
-  //await page.emulate(KnownDevices['iPhone 8']);
 
-  // Go to your site
   await page.goto('https://accounts.google.com');
 
-  await page.waitForSelector('input[type="email"]');
-  await page.waitForSelector('input[type="password"]');
-
-  const elements = await page.$$('input[type="email"], input[type="password"]');
-
-  const boundingBoxes = await Promise.all(
-    elements.map(async (element) => {
-      return element.boundingBox();
-    })
-  );
-
-  boundingBoxes
-    .filter((el) => el)
-    .forEach((bb) => {
-      const bufferLike = Buffer.from(JSON.stringify(bb));
-      ws?.send(bufferLike);
-    });
+  await updateBoundingBoxes(page, ws);
 
   return browser;
 }
