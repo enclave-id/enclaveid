4d3fa79d09284353e48035569713bbbc493c6377
ma9o <magogagiovanni@gmail.com>
2023-12-13 10:55:59 +0100

    Scaffold apps

---
+++ apps/chrome-controller-e2e/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'chrome-controller-e2e',
+  preset: '../..//jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../..//coverage/chrome-controller-e2e',
+};
