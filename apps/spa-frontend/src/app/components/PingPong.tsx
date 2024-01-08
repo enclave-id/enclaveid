bdec26d1d00d3e5564223425a2d21cd965c6c339
David Ripplinger <dcripplinger@gmail.com>
2024-01-08 07:26:16 -0700

    fixed errors so spa-frontend can run
    - removed refs to apollo
    - cleaned up unused code in app.tsx

--- apps/spa-frontend/src/app/components/PingPong.tsx
+++ apps/spa-frontend/src/app/components/PingPong.tsx
@@ -1,24 +1,24 @@
 // This page just tests that the encryption is working e2e
 
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { useState } from 'react';
 
-const doAttestation = async () => {
-  const response = await axios
-    .get('http://localhost:3000/api/attestation', {
-      params: {
-        nonce: 'testNonce123',
-      },
-    })
-    .then((res) => {
-      console.log(res);
+// const doAttestation = async () => {
+//   const response = await axios
+//     .get('http://localhost:3000/api/attestation', {
+//       params: {
+//         nonce: 'testNonce123',
+//       },
+//     })
+//     .then((res) => {
+//       console.log(res);
 
-      return res.data;
-    });
-};
+//       return res.data;
+//     });
+// };
 
 export function PingPong({ title }: { title: string }) {
-  const [started, setStarted] = useState(false);
+  // const [started, setStarted] = useState(false);
 
   return (
     <div>
