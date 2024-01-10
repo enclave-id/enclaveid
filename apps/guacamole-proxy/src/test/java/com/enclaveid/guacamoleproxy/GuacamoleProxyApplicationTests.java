75831aa7b0d8cf099685d3ae456a8ea0e787f9e6
ma9o <magogagiovanni@gmail.com>
2024-01-10 12:52:22 +0100

    Scaffold guacamole-proxy

---
+++ apps/guacamole-proxy/src/test/java/com/enclaveid/guacamoleproxy/GuacamoleProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enclaveid.guacamoleproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuacamoleProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
