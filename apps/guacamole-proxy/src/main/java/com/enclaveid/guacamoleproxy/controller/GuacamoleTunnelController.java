d5d6cfe44e12378ddbd072a4a8b257ca9f30f8ea
ma9o <magogagiovanni@gmail.com>
2024-01-11 09:56:55 +0100

    [guacamole-proxy] Fix a few things so that the app compiles

---
+++ apps/guacamole-proxy/src/main/java/com/enclaveid/guacamoleproxy/controller/GuacamoleTunnelController.java
@@ -0,0 +1,43 @@
+package com.enclaveid.guacamoleproxy.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import org.apache.guacamole.GuacamoleException;
+import org.apache.guacamole.net.GuacamoleSocket;
+import org.apache.guacamole.net.GuacamoleTunnel;
+import org.apache.guacamole.net.InetGuacamoleSocket;
+import org.apache.guacamole.net.SimpleGuacamoleTunnel;
+import org.apache.guacamole.protocol.ConfiguredGuacamoleSocket;
+import org.apache.guacamole.protocol.GuacamoleConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tunnel")
+public class GuacamoleTunnelController {
+
+    @Value("${guacd.host:localhost}")
+    private String guacdHost;
+
+    @Value("${guacd.port:4822}")
+    private int guacdPort;
+
+    @GetMapping
+    public GuacamoleTunnel connect(HttpServletRequest request) throws GuacamoleException {
+        // VNC connection information
+        GuacamoleConfiguration config = new GuacamoleConfiguration();
+        config.setProtocol("vnc");
+        config.setParameter("hostname", "localhost");
+        config.setParameter("port", "5901");
+        config.setParameter("password", "potato");
+
+        // Connect to guacd, proxying a connection to the VNC server above
+        GuacamoleSocket socket = new ConfiguredGuacamoleSocket(
+                new InetGuacamoleSocket(guacdHost, guacdPort),
+                config);
+
+        // Create tunnel from now-configured socket
+        return new SimpleGuacamoleTunnel(socket);
+    }
+}
