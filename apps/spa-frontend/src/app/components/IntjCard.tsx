aad3ecfbdcf53e577e0d89b1e50dba8be3e277fe
mcagdas <mcagdasbayirli@gmail.com>
2024-02-16 16:29:16 +0300

    intj card fixes

--- apps/spa-frontend/src/app/components/IntjCard.tsx
+++ apps/spa-frontend/src/app/components/IntjCard.tsx
@@ -1,20 +1,26 @@
-function IntjCard() {
+import { Button } from './Button';
+
+interface IntjCardProps {
+  header: string;
+  title: string;
+  description: string;
+}
+
+function IntjCard({ header, title, description }: IntjCardProps) {
   return (
     <div className="flex flex-col gap-2.5 w-full">
       <span className="text-[#6C7A8A] text-lg leading-[21px] text-center">
-        MBTI
+        {header}
       </span>
       <article className="border border-[#E5E8EE] rounded-3xl bg-white">
         <div className="flex flex-col gap-[5px] px-3 pt-[9px] pb-[15px] ">
           <h1 className="text-[64px] leading-[75px] text-[#30A78A] text-center">
-            INTJ
+            {title}
           </h1>
-          <p className="text-[#6C7A8A] leading-[22px] px-3">
-            Introverted, Intuitive, Thinking and Judging. It indicates a person
-            who is energized by spending time alone, prioritizes ideas and
-            concepts over facts and details, makes decisions based on logic and
-            reason and prefers to be spontaneous and flexible.
-          </p>
+          <p className="text-[#6C7A8A] leading-[22px] px-3 ">{description}</p>
+          <div>
+            <Button label="Dive Deeper" variant="tertiary" fullWidth />
+          </div>
         </div>
       </article>
     </div>
