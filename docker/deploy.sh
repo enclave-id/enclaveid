730e6d41d1a711d29b65b617b69af1b6c0842986
ma9o <magogagiovanni@gmail.com>
2024-03-02 09:25:18 +0100

    deploy.sh port binding

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -46,7 +46,7 @@ start_proxy() {
     ./gvproxy -listen vsock://:1024 -listen unix:///tmp/network.sock & 
 
     # Expose the port
-    curl --unix-socket /tmp/network.sock http:/unix/services/forwarder/expose -X POST -d '{"local":":443","remote":"192.168.127.2:443"}' 
+    curl --unix-socket /tmp/network.sock http:/unix/services/forwarder/expose -X POST -d '{"local":":8443","remote":"192.168.127.2:443"}' 
 }
 
 # Function to run the enclave
