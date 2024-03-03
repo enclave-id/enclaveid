cddfd8447d1dbfb3fd6161e050d1d2916e0c1643
ma9o <magogagiovanni@gmail.com>
2024-03-03 16:54:47 +0100

    Attestation

--- apps/spa-frontend/src/app/hooks/useTpmEncryptionKey.ts
+++
@@ -1,7 +0,0 @@
-import { useValidateAttestation } from './useValidateAttestation';
-
-export function useTpmEncryptionKey(rawAttestationJwt: string) {
-  const parsedJwt = useValidateAttestation(rawAttestationJwt);
-
-  return { tpmEncryptionKey: parsedJwt.tpmEncryptionKey };
-}
--- apps/spa-frontend/src/app/utils/attestation.ts
+++ apps/spa-frontend/src/app/utils/attestation.ts
@@ -1,28 +1,16 @@
-import * as jose from 'jose';
+import { decode } from 'cbor-x/decode';
+import { base64ToUint8array } from './typeConversion';
 
-export function fetch(attestationJwt: string): CryptoKey {
-  //const decoded = jwt.decode(attestationJwt, { complete: true });
+export function decodeAttestation(base64Attestation: string) {
+  const cborBytes = base64ToUint8array(base64Attestation);
 
-  const decoded = jose.decodeJwt(attestationJwt);
+  const cose = decode(cborBytes);
 
-  console.log(decoded);
+  const attestation = decode(cose);
 
-  return new CryptoKey();
-}
+  const pcr0 = Buffer.from(attestation.pcrs['0']).toString('hex');
+  const nonce = Buffer.from(attestation.nonce).toString('hex');
+  const publicKeyDigest = Buffer.from(attestation.public_key).toString('hex');
 
-export async function validateAttestationJwt(attestationJwt: string): Promise<{
-  tpmEncryptionKey: CryptoKey;
-}> {
-  const publicKey = await jose.importJWK(jwk, alg);
-
-  const { payload, protectedHeader } = await jose.jwtVerify(
-    attestationJwt,
-    publicKey,
-    {
-      issuer: 'urn:example:issuer',
-      audience: 'urn:example:audience',
-    }
-  );
-
-  return true;
+  return { pcr0, nonce, publicKeyDigest };
 }
