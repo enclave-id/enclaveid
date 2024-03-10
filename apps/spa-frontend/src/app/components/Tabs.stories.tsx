530509e45945a11a7d905c738a7a09d9e922bcfd
mcagdas <mcagdasbayirli@gmail.com>
2024-03-10 23:20:08 +0300

    storybook fixes for traitcard, traitcard1 and tabs

--- apps/spa-frontend/src/app/components/Tabs.stories.tsx
+++ apps/spa-frontend/src/app/components/Tabs.stories.tsx
@@ -1,9 +1,12 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Tabs } from './Tabs';
+import { withRouter } from 'storybook-addon-react-router-v6';
+
 
 export default {
   title: 'Components/Tabs',
   component: Tabs,
+  decorators: [withRouter],
 } as Meta;
 
 export const Default: StoryObj = {
