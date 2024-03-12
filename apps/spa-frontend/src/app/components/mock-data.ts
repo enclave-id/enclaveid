738368764119a9bb94770924055e9c9a003bfec0
mcagdas <mcagdasbayirli@gmail.com>
2024-03-12 14:15:24 +0300

    intj card mobile view

--- apps/spa-frontend/src/app/components/mock-data.ts
+++ apps/spa-frontend/src/app/components/mock-data.ts
@@ -1,34 +1,38 @@
 export const traitCard1 = {
-  title: "Ocean",
+  title: 'Ocean',
   data: [
     {
-      label: "Openness to Experiences",
+      label: 'Openness to Experiences',
       value: 95,
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     },
     {
-      label: "Conscientiousness",
+      label: 'Conscientiousness',
       value: 30,
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     },
     {
-      label: "Extraversion",
+      label: 'Extraversion',
       value: 5,
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     },
     {
-      label: "Agreeableness",
+      label: 'Agreeableness',
       value: 50,
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     },
     {
-      label: "Neuroticism",
+      label: 'Neuroticism',
       value: 75,
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     },
   ],
 };
-
 
 export const traitCard2 = [
   {
@@ -96,3 +100,43 @@ export const traitCard2 = [
     value: 30,
   },
 ];
+
+export const intjCard = {
+  header: 'MBTI',
+  label: 'INTJ',
+  description:
+    'Introverted, Intuitive, Thinking and Judging. It indicates a person who is energized by spending time alone, prioritizes ideas and concepts over facts and details, makes decisions based on logic and reason and prefers to be spontaneous and flexible.',
+  data: {
+    title: 'Myers-Briggs Type Indicator',
+    content: `
+    <div style="color: #6C7A8A; line-height: 22.4px; margin-top: 20px;">
+  <p>
+    This personality type acronym stands for <b>Introverted</b>, <b>Intuitive</b>,<b>Thinking</b> and <b>Judging</b>. It indicates a person who is energized by spending time alone, prioritizes ideas and concepts over facts and details, makes decisions based on logic and reason and prefers to be spontaneous and flexible.
+  </p>
+  <div>
+    <p>
+      This combination creates a unique profile:
+    </p>
+    <ul style="list-style-type: decimal; padding-left: 20px; padding-top: 32px; padding-bottom: 32px;">
+      <li>
+        Introverted (I): Prefers solitude for energy and reflection, valuing a rich inner world over extensive social interaction.
+      </li>
+      <li>
+        Intuitive (N): Focused on ideas and concepts rather than concrete details, often imaginative and forward-thinking.
+      </li>
+      <li>
+        Thinking (T): Decisions are made logically and objectively, with a focus on rationality over emotions.
+      </li>
+      <li>
+        Judging (J): Prefers structure and order, enjoys planning and organization, contrary to the notion of being spontaneous and flexible.
+      </li>
+    </ul>
+  </div>
+  <p>
+    INTJs are strategic thinkers, often excelling in roles requiring innovation and planning. They value truth and efficiency but may struggle with emotional aspects of personal relationships.
+  </p>
+</div>
+
+    `,
+  },
+};
