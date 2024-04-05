78ee436a70cbb6ac0c872750e57edab3ac2c6de6
ma9o <magogagiovanni@gmail.com>
2024-04-05 20:45:54 +0200

    Add cloc script

--- k8s/scripts/render_chart_dev.sh
+++ k8s/scripts/render_chart_dev.sh
@@ -8,7 +8,6 @@ LOCAL_REGISTRY="localhost:32000"
 API_IMAGE_DIGEST=$(skopeo inspect "docker://${LOCAL_REGISTRY}/api:${RELEASE_NAME}" --tls-verify=false | jq -r .Digest)
 CREATE_SECRET_IMAGE_DIGEST=$(skopeo inspect "docker://${LOCAL_REGISTRY}/create-secrets:${RELEASE_NAME}" --tls-verify=false | jq -r .Digest)
 
-# We use the distro's helm rather than microk8s
 helm template "${RELEASE_NAME}" "$SCRIPT_DIR"/../helm \
   --set images.api.tag="${API_IMAGE_DIGEST}" \
   --set initContainers.createSecrets.tag="${CREATE_SECRET_IMAGE_DIGEST}" \
