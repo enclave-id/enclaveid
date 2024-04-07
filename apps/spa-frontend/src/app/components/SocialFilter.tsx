d1162908783ba222c84c21907045c71b09068523
mcagdas <mcagdasbayirli@gmail.com>
2024-04-07 15:11:10 +0300

    filtering on social page

---
+++ apps/spa-frontend/src/app/components/SocialFilter.tsx
@@ -0,0 +1,107 @@
+import { Menu, Transition } from '@headlessui/react';
+import { Dispatch, Fragment } from 'react';
+import { RemoveIcon, SearchIcon } from './Icons';
+import { userData } from './mock-data';
+
+interface FilterProps {
+  selectedFilters: string[];
+  setSelectedFilters: Dispatch<React.SetStateAction<string[]>>;
+  setSearchQuery: Dispatch<React.SetStateAction<string>>;
+}
+
+function SocialFilter({
+  selectedFilters,
+  setSelectedFilters,
+  setSearchQuery,
+}: FilterProps) {
+  const uniqueFilterTypes = Array.from(
+    new Set(userData.map((user) => user.type)),
+  );
+
+  const toggleFilter = (filter: string) => {
+    setSelectedFilters((filters) =>
+      filters.includes(filter)
+        ? filters.filter((f) => f !== filter)
+        : [...filters, filter],
+    );
+  };
+
+  const clearFilters = () => setSelectedFilters([]);
+
+  return (
+    <div className="flex flex-col gap-3.5">
+      <div className="flex items-center justify-between">
+        <div className="max-w-[340px] w-full relative ">
+          <input
+            type="text"
+            placeholder="Search for people or activities..."
+            className="outline outline-1 outline-[#E5E8EE] text-passiveLinkColor font-sm leading-4 py-2 pl-8 pr-4 w-full rounded-md"
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button className="pointer-events-none absolute top-1/2 -translate-y-1/2 left-2.5">
+            <SearchIcon />
+          </button>
+        </div>
+        <div className="flex gap-2.5 items-center">
+          {selectedFilters.length > 0 && (
+            <button
+              onClick={clearFilters}
+              className="font-medium text-sm leading-4 text-greenBg outline outline-1 outline-greenBg rounded-md py-2 px-3"
+            >
+              Clear Filters
+            </button>
+          )}
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <Menu.Button className="inline-flex w-full justify-center rounded-md focus:outline-none bg-greenBg py-2 px-8 outline outline-1 outline-[#DBDBDB] text-white font-medium text-sm leading-4">
+                Filters
+              </Menu.Button>
+            </div>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                {uniqueFilterTypes.map((filter) => (
+                  <div className="px-1 py-1" key={filter}>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          onClick={() => toggleFilter(filter)}
+                          className={`group flex w-full items-center rounded-md px-2 py-2 text-sm ${active ? 'bg-greenBg text-white' : 'text-gray-900'}`}
+                        >
+                          {filter}
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </div>
+                ))}
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </div>
+      </div>
+      <div className="flex items-center justify-end gap-2.5">
+        {selectedFilters.map((filter) => (
+          <button
+            key={filter}
+            className="flex items-center gap-2 px-2 py-1.5 outline outline-1 outline-[#E5E8EE] rounded-full"
+            onClick={() => toggleFilter(filter)}
+          >
+            <span className="text-passiveLinkColor font-medium leading-[14px] text-xs">
+              Similar on {filter.toUpperCase()}
+            </span>
+            <RemoveIcon />
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export { SocialFilter };
--- apps/spa-frontend/src/app/components/SocialPage.tsx
+++ apps/spa-frontend/src/app/components/SocialPage.tsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
 import { Breadcrumb } from './Breadcrumb';
 import { Sidebar } from './Sidebar';
 import { SocialCard } from './SocialCard';
-
+import { SocialFilter } from './SocialFilter';
+import { userData } from './mock-data';
 function SocialPage() {
+  const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filteredUsers = userData.filter(
+    (user) =>
+      (selectedFilters.length === 0 || selectedFilters.includes(user.type)) &&
+      (user.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        user.location.toLowerCase().includes(searchQuery.toLowerCase())),
+  );
   return (
     <div className="h-screen bg-white flex sm:flex-row flex-col">
       <Sidebar />
@@ -10,8 +21,17 @@ function SocialPage() {
         <div className="pt-[54px] pb-4 sm:block hidden px-6">
           <Breadcrumb />
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 px-6 gap-x-5 gap-y-4">
-          <SocialCard />
+        <div className="flex flex-col py-3.5 px-6 gap-3.5">
+          <SocialFilter
+            selectedFilters={selectedFilters}
+            setSelectedFilters={setSelectedFilters}
+            setSearchQuery={setSearchQuery}
+          />
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-5 gap-y-4">
+            {filteredUsers.map((user, index) => (
+              <SocialCard key={index} {...user} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
