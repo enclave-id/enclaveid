5585876a6ead938cb8922bcec502cb4178431744
frusciante189 <53580986+frusciante189@users.noreply.github.com>
2024-04-09 17:39:19 +0300

    filtering on social page (#33)

--- apps/spa-frontend/src/app/components/SocialCard.tsx
+++ apps/spa-frontend/src/app/components/SocialCard.tsx
@@ -1,20 +1,28 @@
 import { LocationPinIcon } from './Icons';
 
-function SocialCard() {
+type User = {
+  name: string;
+  gender: string;
+  location: string;
+  type: string;
+  image: string;
+};
+
+function SocialCard({ name, gender, location, image }: User) {
   return (
     <article className="p-6 flex items-center gap-4 border border-[#E5E8EE] rounded-3xl">
-      <img src="./s1.png" alt="" className="w-[101px] h-[101px] rounded-full" />
+      <img src={image} alt="" className="w-[101px] h-[101px] rounded-full" />
       <div className="flex flex-col">
         <h4 className="text-passiveLinkColor font-medium text-2xl leading-7">
-          John Doe
+          {name}
         </h4>
         <h5 className="text-passiveLinkColor font-medium leading-[18px] mt-[9px]">
-          (he/him)
+          {gender}
         </h5>
         <div className="flex items-center gap-2 mt-3">
           <LocationPinIcon />
           <h6 className="text-passiveLinkColor font-medium leading-[18px]">
-            San Francisco
+            {location}
           </h6>
         </div>
       </div>
