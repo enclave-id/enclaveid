51994892d57546d43a8a82b2ecd4811f491f68cb
ma9o <magogagiovanni@gmail.com>
2024-04-09 15:30:49 +0200

    Envoy sidecar

--- k8s/containers/init/create-secrets/entrypoint.sh
+++ k8s/containers/init/create-secrets/entrypoint.sh
@@ -4,67 +4,63 @@ set -euo pipefail
 
 # See https://github.com/microsoft/confidential-container-demos/blob/main/kafka/setup-key.sh
 
-if [ $# != 2 ] && [ $# != 6 ]; then
-  echo "Usage: $0 <KEY_NAME> <KV_STORE_NAME> [<ENABLE_CONFIDENTIALITY>] [<MANAGED_IDENTITY>] [<MAA_ENDPOINT>] [<WORKLOAD_MEASUREMENT>]"
+if [ $# != 3 ] && [ $# != 6 ]; then
+  echo "Usage: $0 <KEY_NAME> <KV_STORE_NAME> <ENVIRONMENT> [<MANAGED_IDENTITY>] [<MAA_ENDPOINT>] [<WORKLOAD_MEASUREMENT>]"
   exit 1
 fi
 
 KEY_NAME=$1
 KV_STORE_NAME=$2
-ENABLE_CONFIDENTIALITY=$3
+ENVIRONMENT=$3
 MANAGED_IDENTITY=$4
 MAA_ENDPOINT=$5
 WORKLOAD_MEASUREMENT=$6
 
-# We tmpfs to store sensitive data since the disk is not yet encrypted by the sidecar
+# We tmpfs to store sensitive data since the disk is not yet encrypted
 TMPFS_DIR="/tmp"
-cd "$TMPFS_DIR"
+cd "$TMPFS_DIR" || exit
 
-AZURE_AKV_RESOURCE_ENDPOINT=""
-AZURE_KV_TYPE=""
+# Generate master secret
+openssl genrsa -out master-secret.pem 3072
+echo "......Generated master secret"
 
-# In the dev environment, we use the service principal instead of MANAGED_IDENTITY
-if [ "$ENABLE_CONFIDENTIALITY" != "true" ]; then
-  set -o allexport
-  if [ -f /.env ]; then
-    source /.env
-  fi
-  set +o allexport
+# Derive intermediate secret from master secret
+SEED=$(openssl rsa -in master-secret.pem -outform DER | openssl dgst -sha256 -binary | xxd -p | tr -d '\n')
+certtool --generate-privkey --key-type=rsa --to-rsa --sec-param=high --seed="$SEED" --provable --outfile intermediate-secret.pem
+echo "......Generated intermediate secret"
 
-  if [ -z "$AZURE_CLIENT_ID" ] || [ -z "$AZURE_CLIENT_SECRET" ] || [ -z "$AZURE_TENANT_ID" ]; then
-    echo "Azure service principal credentials are not set. Please set them in the /.env file."
+# Generate CA from intermediate secret
+openssl req -new -x509 -days 3650 -key intermediate-secret.pem -out ca-cert.pem -config /ca.cnf
+echo "......Generated CA from intermediate secret"
+
+# Upload secret and CA to key vault
+if [ "$ENVIRONMENT" = "microk8s" ]; then
+
+  # Use the unencrypted shared storage to store the secrets
+  # as we would do in AKV
+  cp ./master-secret.pem /mnt/unencrypted-shared-storage/
+  cp ./ca-cert.pem /mnt/unencrypted-shared-storage/
+
+elif [ "$ENVIRONMENT" = "aks" ]; then
+
+  # TODO: Create mHSM and throw away the secrets
+  # TODO: Check that the retention period == key valdity period AND purge protection is enabled (which is immutable)
+
+  AZURE_AKV_RESOURCE_ENDPOINT="$KV_STORE_NAME.managedhsm.azure.net"
+  AZURE_KV_TYPE="managedHSM"
+
+  key_vault_name=$(echo "$AZURE_AKV_RESOURCE_ENDPOINT" | cut -d. -f1)
+  echo "Key vault name is ${key_vault_name}"
+
+  if [[ $(az keyvault list -o json | grep "Microsoft.KeyVault/${AZURE_KV_TYPE}s/${key_vault_name}") ]] 2>/dev/null; then
+    echo "AKV endpoint OK"
+  else
+    echo "Azure akv resource endpoint doesn't exist. Please refer to documentation instructions to set it up first:"
     exit 1
   fi
 
-  az login --service-principal -u "$AZURE_CLIENT_ID" -p "$AZURE_CLIENT_SECRET" --tenant "$AZURE_TENANT_ID"
+  policy_file_name="/key-release-policy.json"
 
-  echo "......Logged in to Azure with dev credentials"
-fi
-
-# TODO: Create mHSM and throw away the secrets
-# TODO: Check that the retention period == key valdity period AND purge protection is enabled (which is immutable)
-
-if [ "$ENABLE_CONFIDENTIALITY" = "true" ]; then
-  AZURE_AKV_RESOURCE_ENDPOINT="$KV_STORE_NAME.managedhsm.azure.net"
-  AZURE_KV_TYPE="managedHSM"
-else
-  AZURE_AKV_RESOURCE_ENDPOINT="$KV_STORE_NAME.vault.azure.net"
-  AZURE_KV_TYPE="vault"
-fi
-
-key_vault_name=$(echo "$AZURE_AKV_RESOURCE_ENDPOINT" | cut -d. -f1)
-echo "Key vault name is ${key_vault_name}"
-
-if [[ $(az keyvault list -o json | grep "Microsoft.KeyVault/${AZURE_KV_TYPE}s/${key_vault_name}") ]] 2>/dev/null; then
-  echo "AKV endpoint OK"
-else
-  echo "Azure akv resource endpoint doesn't exist. Please refer to documentation instructions to set it up first:"
-  exit 1
-fi
-
-policy_file_name="/key-release-policy.json"
-
-if [ "$ENABLE_CONFIDENTIALITY" = "true" ]; then
   # Update the policy file with the workload measurement
   jq --arg equalsValue "${WORKLOAD_MEASUREMENT}" '
             .anyOf[] |= (.allOf |= map(if .claim == "x-ms-sevsnpvm-hostdata" then .equals = $equalsValue else . end))
@@ -76,30 +72,17 @@ if [ "$ENABLE_CONFIDENTIALITY" = "true" ]; then
   jq --arg equalsValue "${MAA_ENDPOINT}" '.anyOf[] |= (.authority = $equalsValue)' "${policy_file_name}" >tmp.$$.json
 
   mv tmp.$$.json "${policy_file_name}"
-else
-  echo "Warning: Confidentiality is disabled. Key will be released to any principal."
-fi
 
-# Create RSA key directly in Azure Key Vault
-if [ "$ENABLE_CONFIDENTIALITY" = "true" ]; then
   echo "......Creating RSA key in ${AZURE_AKV_RESOURCE_ENDPOINT} with HSM"
   az keyvault key create --id https://$AZURE_AKV_RESOURCE_ENDPOINT/keys/${KEY_NAME} --ops \
     wrapKey unwrapKey encrypt decrypt --kty RSA-HSM --size 3072 --exportable \
     --policy ${policy_file_name}
+  echo "......Created RSA key in ${AZURE_AKV_RESOURCE_ENDPOINT}"
+
+  # Upload CA to AKV
+  az keyvault secret set --vault-name "$KV_STORE_NAME" --name $CA_CERT_NAME --file your-ca-cert.pem
+  echo "......Uploaded CA to AKV"
 else
-  echo "......Creating RSA key in ${AZURE_AKV_RESOURCE_ENDPOINT} without HSM"
-  az keyvault key create --id https://$AZURE_AKV_RESOURCE_ENDPOINT/keys/${KEY_NAME} --ops \
-    wrapKey unwrapKey encrypt decrypt --kty RSA --size 3072
+  echo "Unknown environment $ENVIRONMENT. Exiting."
+  exit 1
 fi
-
-echo "......Created RSA key in ${AZURE_AKV_RESOURCE_ENDPOINT}"
-
-# Release the key to the managed identity
-az keyvault key release --id https://$AZURE_AKV_RESOURCE_ENDPOINT/keys/${KEY_NAME} --recipient-object-id $MANAGED_IDENTITY
-echo "......Released key to managed identity"
-
-# Generate CA from master secret
-openssl req -new -x509 -days 3650 -key master-secret.pem -out ca-cert.pem -config /ca.cnf
-# Upload CA to AKV
-az keyvault secret set --vault-name "$KV_STORE_NAME" --name $CA_CERT_NAME --file your-ca-cert.pem
-echo "......Uploaded CA to AKV"
