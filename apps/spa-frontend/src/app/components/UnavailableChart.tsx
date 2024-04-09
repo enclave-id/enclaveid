eb6df989bd85e894f8315f4b3998515e3399d62e
mcagdas <mcagdasbayirli@gmail.com>
2024-04-09 19:45:18 +0300

    removing the lines

--- apps/spa-frontend/src/app/components/UnavailableChart.tsx
+++ apps/spa-frontend/src/app/components/UnavailableChart.tsx
@@ -1,9 +1,7 @@
 function UnavailableChart() {
   return (
     <div className="absolute inset-0">
-      <div className="h-2 bg-black/80 absolute top-1/2 -translate-y-1/2 w-full" />
-      <div className="h-full bg-black/80 absolute left-1/2 -translate-x-1/2 w-2" />
-      <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-[#F3F5F7] z-10 text-3xl w-[80%] mx-auto py-10 text-center">
+      <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-gray-200 shadow-xl rounded-md z-10 text-3xl w-2/3 mx-auto py-10 text-center">
         Unavailable
       </div>
     </div>
