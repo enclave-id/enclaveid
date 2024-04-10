39e0f53e8276adb401cedefa23c13de9d88aeecf
ma9o <magogagiovanni@gmail.com>
2024-04-10 10:48:02 +0200

    Unavailable chart styles

--- apps/spa-frontend/src/app/components/UnavailableChart.tsx
+++ apps/spa-frontend/src/app/components/UnavailableChart.tsx
@@ -1,8 +1,8 @@
 function UnavailableChart() {
   return (
     <div className="absolute inset-0">
-      <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-gray-200 shadow-xl rounded-md z-10 text-3xl w-2/3 mx-auto py-10 text-center">
-        Unavailable
+      <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-gray-50 shadow-xl rounded-xl z-10 text-xl w-2/3 mx-auto py-3 text-center">
+        Complete the questionnaires to see this chart
       </div>
     </div>
   );
