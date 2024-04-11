42aec50dbbfaf959a4a2f78d67cfaa727c7e6016
ma9o <magogagiovanni@gmail.com>
2024-04-11 10:54:57 +0200

    Fix initcontainers args

--- k8s/containers/init/load-secrets/entrypoint.sh
+++ k8s/containers/init/load-secrets/entrypoint.sh
@@ -1,5 +1,16 @@
 set -euo pipefail
 
+if [ $# != 2 ] && [ $# != 4 ]; then
+  echo "Usage: $0 <ENVIRONMENT> <KEY_NAME> [<KV_STORE_NAME>] [<MANAGED_IDENTITY>]"
+  exit 1
+fi
+
+ENVIRONMENT=$1
+KEY_NAME=$2
+
+KV_STORE_NAME=$3
+MANAGED_IDENTITY=$4
+
 # We tmpfs to store sensitive data since the disk is not yet encrypted
 TMPFS_DIR="/tmp"
 cd "$TMPFS_DIR"
@@ -7,7 +18,7 @@ cd "$TMPFS_DIR"
 if [ "$ENVIRONMENT" = "microk8s" ]; then
 
   # Copy the secrets from the unencrypted shared storage to tmpfs
-  cp /mnt/unencrypted-shared-storage/master-secret.pem ./
+  cp "/mnt/unencrypted-shared-storage/$KEY_NAME.pem" ./
   cp /mnt/unencrypted-shared-storage/ca-cert.pem ./
 
 elif
@@ -40,5 +51,6 @@ fi
 
 # Self issue certificates using the CA
 openssl req -new -key intermediate-secret.pem -out intermediate.csr -config /ca.cnf
-openssl x509 -req -days 3650 -in intermediate.csr -CA ca-cert.pem -CAkey intermediate-secret.pem -CAcreateserial -out tls-cert.pem -extfile ca.cnf -extensions v3_req
+openssl x509 -req -days 3650 -in intermediate.csr -CA ca-cert.pem -CAkey intermediate-secret.pem -CAcreateserial -out tls-cert.pem -extfile ca.cnf
+#TODO \ -extensions v3_req
 echo "......Generated self signed TLS certificate"
