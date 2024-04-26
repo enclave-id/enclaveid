2063e4fac016a01010f092e2df3362e3a2080f75
mcagdas <mcagdasbayirli@gmail.com>
2024-04-26 14:31:42 +0300

    sidebarsection story fix

--- apps/spa-frontend/src/app/components/SidebarSection.stories.tsx
+++ apps/spa-frontend/src/app/components/SidebarSection.stories.tsx
@@ -2,16 +2,18 @@ import { Meta, StoryObj } from '@storybook/react';
 import { SidebarSection } from './SidebarSection';
 import { SidebarItem } from './SidebarItem';
 import { HomeIcon, ChartBarIcon } from '@heroicons/react/24/outline';
+import { withRouter } from 'storybook-addon-react-router-v6';
 
 export default {
   title: 'Components/SidebarSection',
   component: SidebarSection,
+  decorators: [withRouter],
 } as Meta<typeof SidebarSection>;
 
 export const WithSidebarItems: StoryObj<typeof SidebarSection> = {
   render: () => (
     <SidebarSection title={'Data'}>
-      <SidebarItem icon={<HomeIcon />} text="Trait Dashboard" active={true} />
+      <SidebarItem icon={<HomeIcon />} text="Trait Dashboard" />
       <SidebarItem icon={<ChartBarIcon />} text="Charts" />
     </SidebarSection>
   ),
