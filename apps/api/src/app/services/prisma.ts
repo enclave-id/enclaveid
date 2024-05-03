9ef942a9344784559ed7d559645d88d875c5526b
ma9o <magogagiovanni@gmail.com>
2024-05-03 19:54:58 +0200

    Messed up env vars

--- apps/api/src/app/services/prisma.ts
+++ apps/api/src/app/services/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 export const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: process.env.DATABASE_URL,
+      url: process.env.API_DATABASE_URL,
     },
   },
 });
