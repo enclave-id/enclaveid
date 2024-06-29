5b52168383dba804b71a1619c480da85ce7cb924
ma9o <46647883+ma9o@users.noreply.github.com>
2024-06-29 13:14:06 +0200

    Interests optimization (#57)
    * [data-pipeline] Fix interests generation
    
    * [data-pipeline] nit

--- apps/data-pipeline/data_pipeline/utils/old_history_utils.py
+++ apps/data-pipeline/data_pipeline/utils/old_history_utils.py
@@ -1,9 +1,8 @@
 import datetime
-import gc
 import re
 from dataclasses import dataclass
 from logging import Logger
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Dict, List, Optional
 
 import polars as pl
 from pydantic import BaseModel, Field
@@ -14,13 +13,11 @@ if is_cuda_available() or TYPE_CHECKING:
     import torch
     from sentence_transformers import SentenceTransformer
     from vllm import LLM, SamplingParams
-    # from vllm.model_executor.parallel_utils.parallel_state import destroy_model_parallel
 else:
     torch = None
     SentenceTransformer = None
     LLM = None
     SamplingParams = None
-    # destroy_model_parallel = None
 
 
 class InterestsSpec(BaseModel):
@@ -36,59 +33,54 @@ class FullHistorySessionsOutput:
 
 
 @dataclass
-class DailyInterestsGenerator:
-    date: datetime.date
-    df: pl.DataFrame
+class InterestsGenerator:
+    daily_dfs: Dict[datetime.date, pl.DataFrame]
     first_instruction: str
     second_instruction: str
     llm: LLM
     sampling_params: SamplingParams
     chunk_size: int = 15
 
-    def _extract_interests_list(self, text):
+    def _extract_interests_list(self, text: str) -> Optional[List[str]]:
         match = re.search(r"\[(.*?)\]", text)
         if match:
             # If a match is found, split the substring by comma
             interests = match.group(1).replace('"', "").replace("'", "").split(",")
-            return [s.strip() for s in interests]
-        else:
-            return None
+            return [interest.strip() for interest in interests]
+        return None
 
     def _generate_chunks(self):
-        # Keep only the relevant columns
-        filtered_df = self.df.select("hour", "title")
+        self.chunks: dict[datetime.date, List[pl.DataFrame]] = {}
 
-        self.chunks: list[pl.DataFrame] = []
-        for frame in filtered_df.iter_slices(n_rows=self.chunk_size):
-            self.chunks.append(frame)
+        for date, day_df in self.daily_dfs.items():
+            self.chunks[date] = []
+            for slice in day_df.iter_slices(self.chunk_size):
+                self.chunks[date].append(slice.select("hour", "title"))
 
     def _generate_chunked_interests(self):
         """
-        TODO: We could potentially see a signifcant speedup by processing all
-        prompts at the same time, instead of daily, where batch sizes can be
-        rather small. This will raise some complexity with respect to mapping
-        responses to their date. We could then potentially also compute the
-        sensitive and general interests at the same time for a further speedup.
-
         TODO: Experiment with prefix caching. See the example below.
         https://github.com/vllm-project/vllm/blob/main/examples/offline_inference_with_prefix.py
         """
+        self.dates = []
         first_prompts: list[str] = []
-        for idx, frame in enumerate(self.chunks, start=1):
-            # Set Polars' string formatting so none of the rows or strings are
-            # compressed / cut off.
-            max_chars = frame["title"].str.len_chars().max()
 
-            with pl.Config(
-                tbl_formatting="NOTHING",
-                tbl_hide_column_data_types=True,
-                tbl_hide_dataframe_shape=True,
-                fmt_str_lengths=max_chars,
-                tbl_rows=-1,
-            ):
-                first_prompts.append(
-                    f"<s> [INST] {self.first_instruction}\n\n{frame}\n [/INST]"
-                )
+        for date, day_dfs in self.chunks.items():
+            for _, frame in enumerate(day_dfs, start=1):
+                # Set Polars' string formatting so none of the rows or strings are compressed / cut off.
+                max_chars = frame["title"].str.len_chars().max()
+
+                with pl.Config(
+                    tbl_formatting="NOTHING",
+                    tbl_hide_column_data_types=True,
+                    tbl_hide_dataframe_shape=True,
+                    fmt_str_lengths=max_chars,
+                    tbl_rows=-1,
+                ):
+                    self.dates.append(date)
+                    first_prompts.append(
+                        f"<s> [INST] {self.first_instruction}\n\n{frame}\n [/INST]"
+                    )
 
         first_requests = self.llm.generate(first_prompts, self.sampling_params)
         first_responses = [resp.outputs[0].text for resp in first_requests]
@@ -111,27 +103,30 @@ class DailyInterestsGenerator:
         for p2, r2 in zip(second_prompts, second_responses):
             self.chunked_convos.append(f"{p2} {r2}</s>")
 
-    def generate_output_record(self):
+    def generate_output_records(self):
         self._generate_chunks()
         self._generate_chunked_interests()
 
-        # Filter out invalid responses
-        valid_chunks = [chunk for chunk in self.chunked_interests if chunk is not None]
+        for date, chunked_interests, chunked_convos in zip(
+            self.dates, self.chunked_interests, self.chunked_convos
+        ):
+            # Filter out invalid responses
+            valid_chunks = [chunk for chunk in chunked_interests if chunk is not None]
 
-        # Flatten the chunks
-        merged_interests = [interest for chunk in valid_chunks for interest in chunk]
+            # Flatten the chunks
+            merged_interests = [
+                interest for chunk in valid_chunks for interest in chunk
+            ]
 
-        return {
-            "date": self.date,
-            "chunked_convos": self.chunked_convos,
-            "chunked_interests": self.chunked_interests,
-            # Different chunks may have the same interests; we only want the
-            # distinct interests across all chunks
-            "interests": list(set(merged_interests)),
-            "count_invalid_responses": (
-                len(self.chunked_interests) - len(valid_chunks)
-            ),
-        }
+            yield {
+                "date": date,
+                "chunked_convos": chunked_convos,
+                "chunked_interests": chunked_interests,
+                # Different chunks may have the same interests; we only want the
+                # distinct interests across all chunks
+                "interests": list(set(merged_interests)),
+                "count_invalid_responses": (len(chunked_interests) - len(valid_chunks)),
+            }
 
 
 def get_full_history_sessions(
@@ -150,28 +145,19 @@ def get_full_history_sessions(
     sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=1024)
 
     daily_records: list[dict[str, Any]] = []
-    for idx, (day, day_df) in enumerate(daily_dfs.items(), start=1):
-        logger.info(f"Processing {day} ({idx}/{len(daily_dfs)})")
-        interests_generator = DailyInterestsGenerator(
-            date=day,
-            df=day_df,
-            first_instruction=first_instruction,
-            second_instruction=second_instruction,
-            llm=llm,
-            sampling_params=sampling_params,
-            chunk_size=chunk_size,
-        )
 
-        daily_records.append(interests_generator.generate_output_record())
+    logger.info(f"Processing {len(daily_dfs)} records")
 
-    logger.info("Unloading the LLM and freeing GPU memory.")
-    # TODO: This is not available anymore in the latest version of vllm
-    # destroy_model_parallel()
-    # del llm.llm_engine.driver_worker  # type: ignore
-    del llm
-    gc.collect()
-    torch.cuda.empty_cache()
-    torch.distributed.destroy_process_group()
+    interests_generator = InterestsGenerator(
+        daily_dfs=daily_dfs,
+        first_instruction=first_instruction,
+        second_instruction=second_instruction,
+        llm=llm,
+        sampling_params=sampling_params,
+        chunk_size=chunk_size,
+    )
+
+    daily_records = interests_generator.generate_output_records()
 
     return FullHistorySessionsOutput(
         output_df=pl.from_records(daily_records),
